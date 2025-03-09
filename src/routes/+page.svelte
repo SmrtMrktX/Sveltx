e3072fcb59d99560ba9e80eca91d6e2771b83070
d3j1x <d3j1x@yahoo.com>
2025-03-09 17:02:38 +0100

    Start Dev

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Sveltx | Free Web Template​</title>
+	<meta
+		name="description"
+		content="Sveltx a modern, feature-rich template for building scalable and performant applications using SvelteKit. This template leverages the latest trends in web development and provides a solid foundation for rapid prototyping or production-ready projects. #opensource 🔥.
+  "
+	/>
+</svelte:head>
