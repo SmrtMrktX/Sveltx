5486cf5e45e7f6a32fe6cf2ae08309a48ce2c6b5
d3j1x <d3j1x@yahoo.com>
2025-03-10 18:08:10 +0100

    add exo-2 font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -59,7 +59,72 @@
 		</ul>
 	</div>
 	<div class="navbar-end">
-		<a href="https://github.com/SmrtMrktX/Sveltx" target="_blank" class="p-2">
+		<div class="dropdown">
+			<div tabindex="0" role="button" class="btn m-1">
+				Theme
+				<svg
+					width="12px"
+					height="12px"
+					class="inline-block h-2 w-2 fill-current opacity-60"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 2048 2048"
+				>
+					<path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+				</svg>
+			</div>
+			<ul tabindex="0" class="dropdown-content bg-base-300 rounded-box z-1 w-52 p-2 shadow-2xl">
+				<li>
+					<input
+						type="radio"
+						name="theme-dropdown"
+						class="theme-controller btn btn-sm btn-block btn-ghost w-full justify-start"
+						aria-label="Default"
+						value="default"
+					/>
+				</li>
+				<li>
+					<input
+						type="radio"
+						name="theme-dropdown"
+						class="theme-controller btn btn-sm btn-block btn-ghost w-full justify-start"
+						aria-label="Light"
+						value="light"
+					/>
+				</li>
+				<li>
+					<input
+						type="radio"
+						name="theme-dropdown"
+						class="theme-controller btn btn-sm btn-block btn-ghost w-full justify-start"
+						aria-label="Dark"
+						value="dark"
+					/>
+				</li>
+				<li>
+					<input
+						type="radio"
+						name="theme-dropdown"
+						class="theme-controller btn btn-sm btn-block btn-ghost w-full justify-start"
+						aria-label="Luxury"
+						value="luxury"
+					/>
+				</li>
+				<li>
+					<input
+						type="radio"
+						name="theme-dropdown"
+						class="theme-controller btn btn-sm btn-block btn-ghost w-full justify-start"
+						aria-label="Black"
+						value="black"
+					/>
+				</li>
+			</ul>
+		</div>
+		<a
+			href="https://github.com/SmrtMrktX/Sveltx"
+			target="_blank"
+			class="mx-2 rounded-sm bg-black p-1"
+		>
 			<svg
 				aria-label="GitHub logo"
 				width="20"
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	const imageUrl = '/src/lib/heroCover.webp';
 </script>
 
 <svelte:head>
@@ -9,3 +10,17 @@
   "
 	/>
 </svelte:head>
+
+<div class="hero min-h-screen" style="background-image: url({imageUrl});">
+	<div class="hero-overlay"></div>
+	<div class="hero-content text-neutral-content text-center">
+		<div class="max-w-md">
+			<h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+			<p class="mb-5">
+				Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+				quasi. In deleniti eaque aut repudiandae et a id nisi.
+			</p>
+			<button class="btn btn-primary">Get Started</button>
+		</div>
+	</div>
+</div>
